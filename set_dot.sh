a7f9a75c1bc7c2fef6d89c0ca0e88cb59cf7226b
Temba Musa Mazingi <tembamazingi@gmail.com>
2023-10-04 10:13:56 +0200

    some refactoring for readability + linking of aliases to functions to reduce the # of similar aliases.

--- set_dot.sh
+++ set_dot.sh
@@ -1,13 +1,21 @@
 #!/bin/zsh
 for dotfile in .aliases .aliases_code .aliases_git .functions .git-prompt.sh .gitexcludes .zprofile .zshrc;
-	do
-		[[ ! -h "$HOME/$dotfile" ]] && { echo "Linking $PWD/$dotfile"; ln -s $PWD/$dotfile $HOME/$dotfile; } || echo "$dotfile already linked.";
+do
+	if [[ ! -h "$HOME/$dotfile" ]]; then
+		echo "Linking $PWD/$dotfile"
+		ln -s $PWD/$dotfile $HOME/$dotfile
+	else
+		echo "$dotfile already linked."
+	fi
 done
 
 if [[ $OSTYPE == darwin* ]]; then
-
-	[[ ! -h "$HOME/.macos" ]] && { echo "Linking $PWD/.macos"; ln -s $PWD/.macos $HOME/.macos; } || echo "$PWD/.macos already linked.";
-
+	if [[ ! -h "$HOME/.macos" ]]; then
+		echo "Linking $PWD/.macos"
+		ln -s $PWD/.macos $HOME/.macos
+	else
+		echo "$PWD/.macos already linked."
+	fi
 fi
 
 exec $SHELL -l
